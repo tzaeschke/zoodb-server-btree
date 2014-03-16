764d211dae0fb7b754f900195c576bff3ed95e10
jonasnick <jonasd.nick@gmail.com>
2014-03-16 23:09:06 +0100

    [new B+ tree] Refactored BTree tests using BTreeFactory.

--- tst/org/zoodb/test/index2/btree/TestBTree.java
+++ tst/org/zoodb/test/index2/btree/TestBTree.java
@@ -97,23 +97,11 @@ public class TestBTree {
 		tree.insert(1, -100);
 
 		// build expected tree
-		BTree expected = new BTree(order);
-		BTreeNode root = new BTreeNode(null, order, false);
-		root.setNumKeys(1);
-		root.setKeys(padLongArray(new long[] { 3 }, order));
-
-		BTreeNode[] children = new BTreeNode[] {
-				new BTreeNode(root, order, true),
-				new BTreeNode(root, order, true) };
-		root.setChildren(children);
-
-		children[0].setKeys(padLongArray(new long[] { 0, 1, 2 }, order));
-		children[0].setValues(padLongArray(new long[] { 5, -100, 5 }, order));
-		children[0].setNumKeys(3);
-		children[1].setKeys(padLongArray(new long[] { 3, 4 }, order));
-		children[1].setValues(padLongArray(new long[] { 1, 10 }, order));
-		children[1].setNumKeys(2);
-		expected.setRoot(root);
+		BTreeFactory factory = new BTreeFactory(order);
+		factory.addInnerLayer(Arrays.asList(Arrays.asList(3L)));
+		factory.addLeafLayer(Arrays.asList(Arrays.asList(pair(0L,5L),pair(1L,-100L),pair(2L,5L)),
+											Arrays.asList(pair(3L,1L),pair(4L,10L))));
+		BTree expected = factory.getTree();
 		
 		assertEquals(
 				"Tree does not have the proper structure after insertion ",
@@ -125,182 +113,52 @@ public class TestBTree {
 	 */
 	@Test
 	public void insertTwoLevelWithSplit() {
-		BTree tree = treeInitialTwoLevelWithSplit();
+		int order = 4;
+		BTreeFactory factory = new BTreeFactory(order);
+		factory.addInnerLayer(Arrays.asList(Arrays.asList(90L)));
+		factory.addInnerLayer(Arrays.asList(Arrays.asList(40L, 60L),
+								Arrays.asList(110L)));
+		factory.addLeafLayerDefault(Arrays.asList(Arrays.asList(10L,20L,30L),
+											Arrays.asList(40L,50L),
+											Arrays.asList(60L,70L,80L),
+											Arrays.asList(90L,100L),
+											Arrays.asList(110L,120L)));
+		BTree tree = factory.getTree();
+
+		factory.clear();
+		factory.addInnerLayer(Arrays.asList(Arrays.asList(90L)));
+		factory.addInnerLayer(Arrays.asList(Arrays.asList(20L,40L,60L),
+								Arrays.asList(110L)));
+		factory.addLeafLayerDefault(Arrays.asList(Arrays.asList(5L,10L),
+											Arrays.asList(20L,30L),
+											Arrays.asList(40L,50L),
+											Arrays.asList(60L,70L,80L),
+											Arrays.asList(90L,100L),
+											Arrays.asList(110L,120L)));
+		BTree expected = factory.getTree();
+
 		tree.insert(5, 5);
-		BTree expected = treeExpectedTwoLevelWithSplit();
 		assertEquals("Tree did not split properly after first insert.",
 				expected, tree);
+		
+		factory.clear();
+		factory.addInnerLayer(Arrays.asList(Arrays.asList(60L,90L)));
+		factory.addInnerLayer(Arrays.asList(Arrays.asList(20L,40L),
+								Arrays.asList(80L),
+								Arrays.asList(110L)));
+		factory.addLeafLayerDefault(Arrays.asList(Arrays.asList(5L,10L),
+											Arrays.asList(20L,30L),
+											Arrays.asList(40L,50L),
+											Arrays.asList(60L,70L),
+											Arrays.asList(80L,85L),
+											Arrays.asList(90L,100L),
+											Arrays.asList(110L,120L)));
+
 		tree.insert(85, 85);
 
-		expected = treeExpectedFinalTwoLevelWithSplit();
+		expected = factory.getTree();
 		assertEquals("Tree did not split properly after second insert.",
 				expected, tree);
-	}
-
-	public BTree treeInitialTwoLevelWithSplit() {
-		int order = 4;
-		BTree tree = new BTree(order);
-		BTreeNode root = new BTreeNode(null, order, false);
-		BTreeNode[] levelOneChildren = new BTreeNode[] {
-				new BTreeNode(root, order, false),
-				new BTreeNode(root, order, false) };
-		root.setNumKeys(1);
-		root.setKeys(padLongArray(new long[] { 90 }, order));
-		root.setChildren(padChildrenArray(levelOneChildren, order));
-
-		levelOneChildren[0].setNumKeys(2);
-		levelOneChildren[0].setKeys(padLongArray(new long[] { 40, 60 }, order));
-
-		levelOneChildren[1].setNumKeys(1);
-		levelOneChildren[1].setKeys(padLongArray(new long[] { 110 }, order));
-
-		BTreeNode[] leaves = new BTreeNode[] {
-				new BTreeNode(levelOneChildren[0], order, true),
-				new BTreeNode(levelOneChildren[0], order, true),
-				new BTreeNode(levelOneChildren[0], order, true),
-				new BTreeNode(levelOneChildren[1], order, true),
-				new BTreeNode(levelOneChildren[1], order, true) };
-
-		levelOneChildren[0].setChildren(padChildrenArray(new BTreeNode[] {
-				leaves[0], leaves[1], leaves[2] }, order));
-		levelOneChildren[1].setChildren(padChildrenArray(new BTreeNode[] {
-				leaves[3], leaves[4] }, order));
-
-		leaves[0].setNumKeys(3);
-		leaves[0].setKeys(padLongArray(new long[] { 10, 20, 30 }, order));
-
-		leaves[1].setNumKeys(2);
-		leaves[1].setKeys(padLongArray(new long[] { 40, 50 }, order));
-
-		leaves[2].setNumKeys(3);
-		leaves[2].setKeys(padLongArray(new long[] { 60, 70, 80 }, order));
-
-		leaves[3].setNumKeys(2);
-		leaves[3].setKeys(padLongArray(new long[] { 90, 100 }, order));
-
-		leaves[4].setNumKeys(2);
-		leaves[4].setKeys(padLongArray(new long[] { 110, 120 }, order));
-
-		tree.setRoot(root);
-		return tree;
-	}
-
-	public BTree treeExpectedTwoLevelWithSplit() {
-		int order = 4;
-		BTree tree = new BTree(order);
-		BTreeNode root = new BTreeNode(null, order, false);
-		BTreeNode[] levelOneChildren = new BTreeNode[] {
-				new BTreeNode(root, order, false),
-				new BTreeNode(root, order, false) };
-		root.setNumKeys(1);
-		root.setKeys(padLongArray(new long[] { 90 }, order));
-		root.setChildren(padChildrenArray(levelOneChildren, order));
-
-		levelOneChildren[0].setNumKeys(3);
-		levelOneChildren[0].setKeys(padLongArray(new long[] { 20, 40, 60 },
-				order));
-
-		levelOneChildren[1].setNumKeys(1);
-		levelOneChildren[1].setKeys(padLongArray(new long[] { 110 }, order));
-
-		BTreeNode[] leaves = new BTreeNode[] {
-				new BTreeNode(levelOneChildren[0], order, true),
-				new BTreeNode(levelOneChildren[0], order, true),
-				new BTreeNode(levelOneChildren[0], order, true),
-				new BTreeNode(levelOneChildren[0], order, true),
-				new BTreeNode(levelOneChildren[1], order, true),
-				new BTreeNode(levelOneChildren[1], order, true) };
-
-		levelOneChildren[0].setChildren(padChildrenArray(new BTreeNode[] {
-				leaves[0], leaves[1], leaves[2], leaves[3] }, order));
-		levelOneChildren[1].setChildren(padChildrenArray(new BTreeNode[] {
-				leaves[4], leaves[5] }, order));
-
-		leaves[0].setNumKeys(2);
-		leaves[0].setKeys(padLongArray(new long[] { 5, 10 }, order));
-		leaves[0].setValues(padLongArray(new long[] { 5, 0, 0 }, order));
-
-		leaves[1].setNumKeys(2);
-		leaves[1].setKeys(padLongArray(new long[] { 20, 30 }, order));
-
-		leaves[2].setNumKeys(2);
-		leaves[2].setKeys(padLongArray(new long[] { 40, 50 }, order));
-
-		leaves[3].setNumKeys(3);
-		leaves[3].setKeys(padLongArray(new long[] { 60, 70, 80 }, order));
-
-		leaves[4].setNumKeys(2);
-		leaves[4].setKeys(padLongArray(new long[] { 90, 100 }, order));
-
-		leaves[5].setNumKeys(2);
-		leaves[5].setKeys(padLongArray(new long[] { 110, 120 }, order));
-
-		tree.setRoot(root);
-		return tree;
-	}
-
-	public BTree treeExpectedFinalTwoLevelWithSplit() {
-		int order = 4;
-		BTree tree = new BTree(order);
-		BTreeNode root = new BTreeNode(null, order, false);
-		BTreeNode[] levelOneChildren = new BTreeNode[] {
-				new BTreeNode(root, order, false),
-				new BTreeNode(root, order, false),
-				new BTreeNode(root, order, false), };
-		root.setNumKeys(2);
-		root.setKeys(padLongArray(new long[] { 60, 90 }, order));
-		root.setChildren(padChildrenArray(levelOneChildren, order));
-
-		levelOneChildren[0].setNumKeys(2);
-		levelOneChildren[0].setKeys(padLongArray(new long[] { 20, 40 }, order));
-
-		levelOneChildren[1].setNumKeys(1);
-		levelOneChildren[1].setKeys(padLongArray(new long[] { 80 }, order));
-
-		levelOneChildren[2].setNumKeys(1);
-		levelOneChildren[2].setKeys(padLongArray(new long[] { 110 }, order));
-
-		BTreeNode[] leaves = new BTreeNode[] {
-				new BTreeNode(levelOneChildren[0], order, true),
-				new BTreeNode(levelOneChildren[0], order, true),
-				new BTreeNode(levelOneChildren[0], order, true),
-				new BTreeNode(levelOneChildren[1], order, true),
-				new BTreeNode(levelOneChildren[1], order, true),
-				new BTreeNode(levelOneChildren[2], order, true),
-				new BTreeNode(levelOneChildren[2], order, true) };
-
-		levelOneChildren[0].setChildren(padChildrenArray(new BTreeNode[] {
-				leaves[0], leaves[1], leaves[2] }, order));
-		levelOneChildren[1].setChildren(padChildrenArray(new BTreeNode[] {
-				leaves[3], leaves[4] }, order));
-		levelOneChildren[2].setChildren(padChildrenArray(new BTreeNode[] {
-				leaves[5], leaves[6] }, order));
-
-		leaves[0].setNumKeys(2);
-		leaves[0].setKeys(padLongArray(new long[] { 5, 10 }, order));
-		leaves[0].setValues(padLongArray(new long[] { 5, 0, 0 }, order));
-
-		leaves[1].setNumKeys(2);
-		leaves[1].setKeys(padLongArray(new long[] { 20, 30 }, order));
-
-		leaves[2].setNumKeys(2);
-		leaves[2].setKeys(padLongArray(new long[] { 40, 50 }, order));
-
-		leaves[3].setNumKeys(2);
-		leaves[3].setKeys(padLongArray(new long[] { 60, 70 }, order));
-
-		leaves[4].setNumKeys(2);
-		leaves[4].setKeys(padLongArray(new long[] { 80, 85 }, order));
-		leaves[4].setValues(padLongArray(new long[] { 0, 85 }, order));
-
-		leaves[5].setNumKeys(2);
-		leaves[5].setKeys(padLongArray(new long[] { 90, 100 }, order));
-
-		leaves[6].setNumKeys(2);
-		leaves[6].setKeys(padLongArray(new long[] { 110, 120 }, order));
-
-		tree.setRoot(root);
-		return tree;
 	}
 	
 	/*
@@ -308,60 +166,17 @@ public class TestBTree {
 	 */
 	@Test
 	public void insertOneLevelWithSplit() {
-		BTree tree = treeInitialOneLevelWithSplit();
-		tree.insert(8, 8);
-		BTree expected = treeExpectedOneLevelWithSplit();
-		assertEquals("Tree did not split properly after insert.",
-				expected, tree);
-	}
-	
-	private BTree treeInitialOneLevelWithSplit() {
 		int order = 5;
-		BTree tree = new BTree(order);
-		BTreeNode root = new BTreeNode(null, order, false);
-		BTreeNode[] levelOneChildren = new BTreeNode[] {
-				new BTreeNode(root, order, true),
-				new BTreeNode(root, order, true),
-				new BTreeNode(root, order, true),
-				new BTreeNode(root, order, true),
-				new BTreeNode(root, order, true) };
+        BTreeFactory factory = new BTreeFactory(order);
+		factory.addInnerLayer(Arrays.asList(Arrays.asList(13L,17L,24L,30L)));
+		factory.addLeafLayerDefault(Arrays.asList(Arrays.asList(2L,3L,5L,7L),
+											Arrays.asList(14L,16L),
+											Arrays.asList(19L,20L,22L),
+											Arrays.asList(24L,27L,29L),
+											Arrays.asList(33L,34L,38L,39L)));
+		BTree tree = factory.getTree();
 
-		root.setNumKeys(4);
-		root.setKeys(padLongArray(new long[] { 13, 17, 24, 30 }, order));
-		root.setChildren(padChildrenArray(levelOneChildren, order));
-
-		levelOneChildren[0].setNumKeys(4);
-		long[] leafOneRecords = new long[] { 2, 3, 5, 7 };
-		levelOneChildren[0].setKeys(padLongArray(leafOneRecords, order));
-		levelOneChildren[0].setValues(padLongArray(leafOneRecords, order));
-
-		levelOneChildren[1].setNumKeys(2);
-		long[] leafTwoRecords = new long[] { 14, 16 };
-		levelOneChildren[1].setKeys(padLongArray(leafTwoRecords, order));
-		levelOneChildren[1].setValues(padLongArray(leafTwoRecords, order));
-
-		levelOneChildren[2].setNumKeys(3);
-		long[] leafThreeRecords = new long[] { 19,20,22 };
-		levelOneChildren[2].setKeys(padLongArray(leafThreeRecords, order));
-		levelOneChildren[2].setValues(padLongArray(leafThreeRecords, order));
-		
-		levelOneChildren[3].setNumKeys(3);
-		long[] leafFourRecords = new long[] { 24,27,29 };
-		levelOneChildren[3].setKeys(padLongArray(leafFourRecords, order));
-		levelOneChildren[3].setValues(padLongArray(leafFourRecords, order));
-		
-		levelOneChildren[4].setNumKeys(4);
-		long[] leafFiveRecords = new long[] { 33,34,38,39 };
-		levelOneChildren[4].setKeys(padLongArray(leafFiveRecords, order));
-		levelOneChildren[4].setValues(padLongArray(leafFiveRecords, order));
-
-		tree.setRoot(root);
-		return tree;
-	}
-	
-	private BTree treeExpectedOneLevelWithSplit() {
-		int order = 5;
-		BTreeFactory factory = new BTreeFactory(order);
+		factory.clear();
 		factory.addInnerLayer(Arrays.asList(Arrays.asList(17L)));
 		factory.addInnerLayer(Arrays.asList(Arrays.asList(5L,13L),
 											Arrays.asList(24L,30L)));
@@ -371,9 +186,11 @@ public class TestBTree {
 											Arrays.asList(19L,20L,22L),
 											Arrays.asList(24L,27L,29L),
 											Arrays.asList(33L,34L,38L,39L)));
+		BTree expected = factory.getTree();
 
-		return factory.getTree();
-
+		tree.insert(8, 8);
+		assertEquals("Tree did not split properly after insert.",
+				expected, tree);
 	}
 	
 	/*
@@ -525,28 +342,6 @@ public class TestBTree {
 		assertEquals(tree4, tree3);
 	}
 
-	private long[] padLongArray(long[] keys, int order) {
-		long[] paddedKeyArray = new long[order - 1];
-		for (int i = 0; i < keys.length; i++) {
-			paddedKeyArray[i] = keys[i];
-		}
-		for (int i = keys.length; i < order - 1; i++) {
-			paddedKeyArray[i] = 0;
-		}
-		return paddedKeyArray;
-	}
-
-	private BTreeNode[] padChildrenArray(BTreeNode[] children, int order) {
-		BTreeNode[] paddedNodeArray = new BTreeNode[order];
-		for (int i = 0; i < children.length; i++) {
-			paddedNodeArray[i] = children[i];
-		}
-		for (int i = children.length; i < order; i++) {
-			paddedNodeArray[i] = null;
-		}
-		return paddedNodeArray;
-	}
-	
 	public static Pair<Long,Long> pair(long x, long y) {
 		return new Pair<Long,Long>(x,y);
 	}
