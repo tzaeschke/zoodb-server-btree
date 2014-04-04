4403672a3daf9e225fe23a2389a5328d526f003d
Bogdan Vancea <bogdan.aurel.vancea@gmail.com>
2014-04-04 22:20:48 +0200

    [new B+ tree] Removed unneeded method.

--- src/org/zoodb/internal/server/index/btree/BTreeStorageBufferManager.java
+++ src/org/zoodb/internal/server/index/btree/BTreeStorageBufferManager.java
@@ -1,18 +1,15 @@
 package org.zoodb.internal.server.index.btree;
 
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Observable;
-
 import org.zoodb.internal.server.DiskIO;
 import org.zoodb.internal.server.DiskIO.DATA_TYPE;
 import org.zoodb.internal.server.StorageChannel;
 import org.zoodb.internal.server.StorageChannelInput;
 import org.zoodb.internal.server.StorageChannelOutput;
-import org.zoodb.internal.server.index.btree.unique.UniquePagedBTree;
 import org.zoodb.internal.util.DBLogger;
-import org.zoodb.test.index2.btree.BTreeFactory;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Observable;
 
 public class BTreeStorageBufferManager implements BTreeBufferManager {
 
@@ -300,20 +297,4 @@ public class BTreeStorageBufferManager implements BTreeBufferManager {
 	public int getStatNReadPages() {
 		return statNReadPages;
 	}
-	
-	public static BTree<PagedBTreeNode> getTestTree(BTreeStorageBufferManager bufferManager) {
-		int order = bufferManager.computeOrder();
-		BTreeFactory factory = new BTreeFactory(order, bufferManager, true);
-		factory.addInnerLayer(Arrays.asList(Arrays.asList(17L)));
-		factory.addInnerLayer(Arrays.asList(Arrays.asList(5L, 13L),
-				Arrays.asList(24L, 30L)));
-		factory.addLeafLayerDefault(Arrays.asList(Arrays.asList(2L, 3L),
-				Arrays.asList(5L, 7L, 8L), Arrays.asList(14L, 16L),
-				Arrays.asList(19L, 20L, 22L), Arrays.asList(24L, 27L, 29L),
-				Arrays.asList(33L, 34L, 38L, 39L)));
-		UniquePagedBTree tree = (UniquePagedBTree) factory.getTree();
-		return tree;
-	}
-
-
 }
