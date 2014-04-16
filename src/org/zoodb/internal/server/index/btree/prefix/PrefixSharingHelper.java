e59ebed80a10050de3a4500025020669ff761299
Bogdan Vancea <bogdan.aurel.vancea@gmail.com>
2014-04-16 14:32:48 +0200

    [prefix sharing] Added splitting of array into 2 equal size arrays.

--- src/org/zoodb/internal/server/index/btree/prefix/PrefixSharingHelper.java
+++ src/org/zoodb/internal/server/index/btree/prefix/PrefixSharingHelper.java
@@ -45,9 +45,61 @@ public class PrefixSharingHelper {
         System.out.println(String.format("First:\t %d\t %-72s",first, toBinaryLongString(first)));
         System.out.println(String.format("Last:\t %d\t %-72s",last, toBinaryLongString(last)));
         System.out.println(String.format("Prefix:\t %d\t %-72s",prefix, toBinaryLongString(first >> (64 - prefix))));
-        return (first >> (64 - prefix));
+        return prefix;
     }
 
+    /**
+     * Computes the optimal split point for a prefix shared array after inserting
+     * a new element newElement. The optimal split point is the index that splits
+     * the array into two prefix shared arrays of relatively equal size.
+     *
+     * @param arr               The prefix shared array.
+     * @return
+     */
+    public static int computePrefixForSplitAfterInsert(long[] arr) {
+        /*
+         *  Perform a binary search by computing the sizes of the left and right array
+         *  after splitting by a certain index.
+         *
+         *  If the left array has a larger size, move the splitting point to the right.
+         */
+        int low = 0 ;
+        int high = arr.length - 1;
+        int mid = 0;
+        int optimalIndex = 0;
+        long optimalDiff = Long.MAX_VALUE;
+        while (low <= high) {
+            mid = low + ((high - low) >> 1);
+            long prefixLeft = computePrefix(arr[0], arr[mid]);
+            long prefixRight = computePrefix(arr[mid+1], arr[arr.length - 1]);
+            long sizeLeft = prefixLeft + (mid + 1) * (64 - prefixLeft);
+            long sizeRight = prefixRight + (arr.length - 1 - mid) * (64 - prefixRight);
+            if (optimalDiff > Math.abs(sizeLeft - sizeRight)) {
+                optimalIndex = mid;
+                optimalDiff = Math.abs(sizeLeft - sizeRight);
+            }
+            if (sizeLeft < sizeRight) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+        System.out.println("Optimal difference: " + optimalDiff);
+        return optimalIndex;
+    }
+
+    public static void printSharedPrefixArray(long[] arr) {
+        long prefix = computePrefix(arr);
+        System.out.println("Prefix size:\t" + prefix);
+        System.out.println("Array size:\t" + (64 - prefix) * arr.length);
+        System.out.println("Total size:\t" + (prefix + ((64 - prefix) * arr.length)));
+    }
+
+    /**
+     * Print a 64 character representation of a long value.
+     * @param number
+     * @return
+     */
     public static String toBinaryLongString(long number) {
         String binaryString = Long.toBinaryString(number);
         int padding = 64 - binaryString.length();
--- tst/org/zoodb/test/index2/btree/TestPrefixSharing.java
+++ tst/org/zoodb/test/index2/btree/TestPrefixSharing.java
@@ -3,6 +3,10 @@ package org.zoodb.test.index2.btree;
 import org.junit.Test;
 import org.zoodb.internal.server.index.btree.prefix.PrefixSharingHelper;
 
+import java.util.Arrays;
+
+import static org.junit.Assert.assertArrayEquals;
+
 public class TestPrefixSharing {
 
     @Test
@@ -11,6 +15,41 @@ public class TestPrefixSharing {
 
         long prefix = PrefixSharingHelper.computePrefix(arr);
         System.out.println(prefix);
+    }
+
+    @Test
+    public void testSplitEqual() {
+        long[] arr = {5, 5, 5, 5, 5, 5};
+        long[] expectedLeft = {5, 5, 5};
+        long[] expectedRight = {5, 5, 5};
+        assertCorrectSplit(arr, expectedLeft, expectedRight);
+    }
+
+    @Test
+    public void testSplitUnbalanced() {
+        long[] arr = {5, 5, 5, 5, 5, 6};
+        long[] expectedLeft = {5, 5, 5, 5, 5};
+        long[] expectedRight = {6};
+        assertCorrectSplit(arr, expectedLeft, expectedRight);
+    }
+
+    @Test
+    public void testSplitLarge() {
+        assertCorrectSplit(new long[] {1,1,2}, new long[] {1, 1}, new long[] {2} );
+    }
+
+    public void assertCorrectSplit(long[] arr, long[] expectedLeft, long[] expectedRight) {
+        int splitIndex = PrefixSharingHelper.computePrefixForSplitAfterInsert(arr);
+        long[] arrLeft = Arrays.copyOfRange(arr, 0, splitIndex + 1);
+        long[] arrRight = Arrays.copyOfRange(arr, splitIndex + 1, arr.length);
+        assertArrayEquals(expectedLeft, arrLeft);
+        assertArrayEquals(expectedRight, arrRight);
+        System.out.println("Detailed split information for array: " + Arrays.toString(arr));
+        System.out.println("Index:\t" + splitIndex);
+        System.out.println("Left:\t" + Arrays.toString(arrLeft));
+        System.out.println("Right:\t" + Arrays.toString(arrRight));
+        PrefixSharingHelper.printSharedPrefixArray(arrLeft);
+        PrefixSharingHelper.printSharedPrefixArray(arrRight);
     }
 
 }
