f69c56ca7e8fdd479876dd60d58660fbe89c715a
Bogdan Vancea <bogdan.aurel.vancea@gmail.com>
2014-05-28 15:50:14 +0200

    [prefix-sharing] Cleanup.

--- src/org/zoodb/internal/server/index/btree/nonunique/NonUniquePagedBTreeNode.java
+++ src/org/zoodb/internal/server/index/btree/nonunique/NonUniquePagedBTreeNode.java
@@ -148,16 +148,16 @@ public class NonUniquePagedBTreeNode extends PagedBTreeNode {
         ret += "]";
 
         if (!isLeaf()) {
-//            ret += "\n\tc:";
-//            if (this.getNumKeys() != 0) {
-//                for (int i = 0; i < this.getNumKeys() + 1; i++) {
-//                    String[] lines = this.getChild(i).toString()
-//                            .split("\r\n|\r|\n");
-//                    for (String l : lines) {
-//                        ret += "\n\t" + l;
-//                    }
-//                }
-//            }
+            ret += "\n\tc:";
+            if (this.getNumKeys() != 0) {
+                for (int i = 0; i < this.getNumKeys() + 1; i++) {
+                    String[] lines = this.getChild(i).toString()
+                            .split("\r\n|\r|\n");
+                    for (String l : lines) {
+                        ret += "\n\t" + l;
+                    }
+                }
+            }
         }
         return ret;
     }
