71a949bcb3d17ecc73f7e71b4e2e27ac01ac07c3
Bogdan Vancea <bogdan.aurel.vancea@gmail.com>
2014-06-04 13:18:08 +0200

    [misc] Added license.

--- src/org/zoodb/internal/server/index/btree/BTreeLeafEntryIterator.java
+++ src/org/zoodb/internal/server/index/btree/BTreeLeafEntryIterator.java
@@ -1,12 +1,32 @@
+/*
+ * Copyright 2009-2014 Tilmann Zaeschke. All rights reserved.
+ *
+ * This file is part of ZooDB.
+ *
+ * ZooDB is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * ZooDB is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with ZooDB.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * See the README and COPYING files for further information.
+ */
 package org.zoodb.internal.server.index.btree;
-
-import java.util.ConcurrentModificationException;
-import java.util.LinkedList;
-import java.util.NoSuchElementException;
 
 import org.zoodb.internal.server.index.LongLongIndex;
 import org.zoodb.internal.server.index.LongLongIndex.LLEntry;
 import org.zoodb.internal.util.DBLogger;
+
+import java.util.ConcurrentModificationException;
+import java.util.LinkedList;
+import java.util.NoSuchElementException;
 
 public abstract class BTreeLeafEntryIterator implements
 		LongLongIndex.LLEntryIterator {
