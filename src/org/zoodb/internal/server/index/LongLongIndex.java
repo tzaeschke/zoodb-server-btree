efb562c715c6b263fd5353cae3e32ec5d798a480
Tilmann <zoodb@gmx.de>
2017-09-20 22:34:03 +0200

    Cleanup

--- src/org/zoodb/internal/server/index/LongLongIndex.java
+++ src/org/zoodb/internal/server/index/LongLongIndex.java
@@ -50,26 +50,6 @@ public interface LongLongIndex {
 		public long getValue() {
 			return value;
 		}
-
-        @Override
-        public boolean equals(Object o) {
-            if (this == o) return true;
-            if (!(o instanceof LLEntry)) return false;
-
-            LLEntry llEntry = (LLEntry) o;
-
-            if (key != llEntry.key) return false;
-            if (value != llEntry.value) return false;
-
-            return true;
-        }
-
-        @Override
-        public int hashCode() {
-            int result = (int) (key ^ (key >>> 32));
-            result = 31 * result + (int) (value ^ (value >>> 32));
-            return result;
-        }
     }
 
 	//Interface for index iterators that can be deregisterd.
